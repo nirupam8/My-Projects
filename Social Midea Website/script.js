6d864519eecb6185054e09f3f7658d0c8b3857b6
nirupam8 <nirmalpaul925@gmail.com>
2024-01-29 20:46:53 +0530

    90% complete

--- Social Midea Website/script.js
+++
@@ -1,9 +0,0 @@
-let currentIndex = 0;
-const slides = document.querySelectorAll('.slide');
-const totalSlides = slides.length;
-
-function moveSlide(direction) {
-  currentIndex = (currentIndex + direction + totalSlides) % totalSlides;
-  const offset = -currentIndex * 100;
-  document.querySelector('.slider').style.transform = `translateX(${offset}%)`;
-}
